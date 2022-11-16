89f73f5a153ab251b30ece5d51e33d2c74ed4229
asgard <1792181243@qq.com>
2022-11-16 16:39:29 +0800

    客户 Service接口

---
+++ src/main/java/com/cqucc/service/customerService.java
@@ -0,0 +1,7 @@
+package com.cqucc.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.cqucc.pojo.Customer;
+
+public interface customerService extends IService<Customer> {
+}
