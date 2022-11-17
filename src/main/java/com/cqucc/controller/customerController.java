2342b105c14cad095792593cfd17ca8e718a5176
asgard <1792181243@qq.com>
2022-11-17 12:09:58 +0800

    客户 controller类

--- src/main/java/com/cqucc/controller/customerController.java
+++ src/main/java/com/cqucc/controller/customerController.java
@@ -3,16 +3,17 @@ package com.cqucc.controller;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.cqucc.pojo.Customer;
 import com.cqucc.service.customerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin
+@Slf4j
 public class customerController {
     @Autowired
     private customerService customerService;
@@ -23,5 +24,14 @@ public class customerController {
         limit = 10;
         IPage<Customer> pageInfo = customerService.getOnePage(page, limit);
         return pageInfo;
+    }
+
+
+    public Map<String ,Object> getCustomers(Integer page, Integer limit){
+        HashMap<String,Object> map=new HashMap<>();
+
+        IPage<Customer> onePage = customerService.getOnePage(page, limit);
+
+        return map;
     }
 }
