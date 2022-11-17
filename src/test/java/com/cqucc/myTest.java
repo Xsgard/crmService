ed87737286c627e0e4391221e305a49a81459a24
asgard <1792181243@qq.com>
2022-11-17 19:32:56 +0800

    update .gitignore

---
+++ src/test/java/com/cqucc/myTest.java
@@ -0,0 +1,13 @@
+package com.cqucc;
+
+import com.cqucc.service.customerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class myTest {
+    @Autowired
+    private customerService customerService;
+
+
+}
