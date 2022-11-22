6c6449ce1860a6a58ace43acc9656ba77b61cc7d
asgard <1792181243@qq.com>
2022-11-22 09:15:37 +0800

    客户销售机会Dao接口

---
+++ src/main/java/com/cqucc/dao/cusChanceDao.java
@@ -0,0 +1,9 @@
+package com.cqucc.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cqucc.pojo.cusChance;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface cusChanceDao extends BaseMapper<cusChance> {
+}
