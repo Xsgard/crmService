76852e678a84e567a8a9715a95bd6538b16c910f
asgard <1792181243@qq.com>
2022-11-22 09:22:25 +0800

    客户销售机会Service实现类

---
+++ src/main/java/com/cqucc/service/Impl/cusChanceServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cqucc.service.Impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cqucc.dao.cusChanceDao;
+import com.cqucc.pojo.CusChance;
+import com.cqucc.service.cusChanceService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class cusChanceServiceImpl extends ServiceImpl<cusChanceDao, CusChance> implements cusChanceService {
+}
