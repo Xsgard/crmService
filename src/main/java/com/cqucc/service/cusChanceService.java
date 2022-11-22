622fe2fc8c0c4c9c92338a322a10b70410f26ea8
asgard <1792181243@qq.com>
2022-11-22 09:53:51 +0800

    客户销售机会Service接口

--- src/main/java/com/cqucc/service/cusChanceService.java
+++ src/main/java/com/cqucc/service/cusChanceService.java
@@ -1,7 +1,9 @@
 package com.cqucc.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.cqucc.pojo.CusChance;
 
 public interface cusChanceService extends IService<CusChance> {
+    IPage<CusChance> getOnePage(Integer page,Integer limit);
 }
