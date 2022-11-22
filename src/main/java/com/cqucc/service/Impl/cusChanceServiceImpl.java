f4742b2a2a8075489e1a10ceeb90c5f7f73b6a71
asgard <1792181243@qq.com>
2022-11-22 09:54:03 +0800

    客户销售机会Service实现类

--- src/main/java/com/cqucc/service/Impl/cusChanceServiceImpl.java
+++ src/main/java/com/cqucc/service/Impl/cusChanceServiceImpl.java
@@ -1,5 +1,8 @@
 package com.cqucc.service.Impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cqucc.dao.cusChanceDao;
 import com.cqucc.pojo.CusChance;
@@ -8,4 +11,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class cusChanceServiceImpl extends ServiceImpl<cusChanceDao, CusChance> implements cusChanceService {
+    /**
+     * 销售机会分页
+     * @param page
+     * @param limit
+     * @return
+     */
+    @Override
+    public IPage<CusChance> getOnePage(Integer page, Integer limit) {
+        Page<CusChance> cusChancePage=new Page<>(page,limit);
+        LambdaQueryWrapper<CusChance> queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.orderByDesc(CusChance::getScCreatetime);
+        this.page(cusChancePage,queryWrapper);
+
+        return cusChancePage;
+    }
 }
