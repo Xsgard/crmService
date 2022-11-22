cabbb216e3ebbeaae8e28821d79214eddd09379e
asgard <1792181243@qq.com>
2022-11-22 09:53:36 +0800

    销售机会实体类

--- src/main/java/com/cqucc/pojo/CusChance.java
+++ src/main/java/com/cqucc/pojo/CusChance.java
@@ -11,7 +11,7 @@ public class CusChance {
 
     private Integer scId;
     //客户名称
-    private String scCusName;
+    private String scCusname;
     //机会来源（0促销、1广告、2搜索引擎、3线上咨询、4电话咨询、5邮件咨询、6客户介绍）
     private Integer scComming;
     //联系人
@@ -25,13 +25,13 @@ public class CusChance {
     //机会描述
     private String scDesc;
     //创建人
-    private Integer scCreateUserId;
+    private Integer scCreateuserid;
     //创建时间
-    private LocalDateTime scCreateTime;
+    private LocalDateTime scCreatetime;
     //指派给谁
-    private Integer scGiveUserId;
+    private Integer scGiveuserid;
     //指派时间
-    private LocalDateTime scGiveTime;
+    private LocalDateTime scGivetime;
     //状态（0未分配、1已分配、2开发成功、3开发失败、4开发中）
     private Integer scStatus;
 
