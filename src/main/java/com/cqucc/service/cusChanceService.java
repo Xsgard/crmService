a6c0a25949e5d9807c03f919ef3e00a46b6e4af3
asgard <1792181243@qq.com>
2022-11-22 09:22:12 +0800

    客户销售机会Service接口

---
+++ src/main/java/com/cqucc/service/cusChanceService.java
@@ -0,0 +1,7 @@
+package com.cqucc.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.cqucc.pojo.CusChance;
+
+public interface cusChanceService extends IService<CusChance> {
+}
