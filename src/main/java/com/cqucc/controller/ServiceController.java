6db65c2ea779010316e4d278cc36f6244119ced5
asgard <1792181243@qq.com>
2022-11-23 20:08:03 +0800

    服务 controller

---
+++ src/main/java/com/cqucc/controller/ServiceController.java
@@ -0,0 +1,16 @@
+package com.cqucc.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.cqucc.service.ServicesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/services")
+public class ServiceController {
+    @Autowired
+    private ServicesService service;
+
+
+}
