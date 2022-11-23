50ab1f1ddbcc26f5ed4ba4e8ea92ebe8fa761a84
asgard <1792181243@qq.com>
2022-11-23 21:31:34 +0800

    客户 controller类

--- src/main/java/com/cqucc/controller/ServiceController.java
+++ src/main/java/com/cqucc/controller/ServiceController.java
@@ -4,15 +4,34 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.cqucc.common.R;
 import com.cqucc.pojo.Services;
 import com.cqucc.service.ServicesService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/services")
+@CrossOrigin
+@Slf4j
 public class ServiceController {
     @Autowired
     private ServicesService service;
 
+    @PostMapping("/add")
+    public R<Services> add(Services services) {
+        //设置客户id
+        services.setCusId(1);
+        //设置创建时间
+        services.setSerCreatetime(LocalDateTime.now());
+        //设置创建人
+        services.setSetCreater(1);
 
+        service.save(services);
+
+        return null;
+    }
 }
