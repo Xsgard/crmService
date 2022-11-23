05b14b201f0d23a13a1a72b8c25a698273322331
liao <2819054979@qq.com>
2022-11-23 21:32:18 +0800

    服务controller

--- src/main/java/com/cqucc/controller/ServiceController.java
+++ src/main/java/com/cqucc/controller/ServiceController.java
@@ -1,18 +1,36 @@
 package com.cqucc.controller;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cqucc.common.R;
-import com.cqucc.pojo.Services;
-import com.cqucc.service.ServicesService;
+import com.cqucc.pojo.Service;
+import com.cqucc.service.ServiceService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/services")
+@Slf4j
 public class ServiceController {
     @Autowired
-    private ServicesService service;
+    private ServiceService serviceService;
 
+    /**
+     * 员工信息分页查询
+     * @param page
+     * @param limit
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(Integer page, Integer limit){
+        log.info("page = {},pageSize = {},name = {}" ,page,limit);
+        //构造分页构造器
+        Page pageInfo = new Page(page,limit);
+        //执行查询
+        serviceService.page(pageInfo);
+
+        return R.success(pageInfo);
+    }
 
 }
