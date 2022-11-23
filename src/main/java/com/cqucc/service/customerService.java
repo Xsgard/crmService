086e10361cc337aa536225862315a5235034e05d
asgard <1792181243@qq.com>
2022-11-23 11:39:54 +0800

    客户销售机会Service接口

--- src/main/java/com/cqucc/service/customerService.java
+++ src/main/java/com/cqucc/service/customerService.java
@@ -4,9 +4,17 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.cqucc.pojo.Customer;
 
+import java.util.List;
+
 public interface customerService extends IService<Customer> {
 
     IPage<Customer> getOnePage(Integer page, Integer limit);
 
     Boolean update(Customer customer);
+
+    boolean add(Customer customer);
+
+    List<Customer> findCusByName(String cusName);
+
+    Boolean deleteByIds(List<Integer> cusIds);
 }
