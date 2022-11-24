fab817727e133a8b5890bf7d8795d3c732fef8d8
asgard <1792181243@qq.com>
2022-11-24 08:56:04 +0800

    服务 controller

--- src/main/java/com/cqucc/controller/ServiceController.java
+++ src/main/java/com/cqucc/controller/ServiceController.java
@@ -1,15 +1,13 @@
 package com.cqucc.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cqucc.common.R;
 import com.cqucc.pojo.Services;
 import com.cqucc.service.ServicesService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -33,5 +31,16 @@ public class ServiceController {
         service.save(services);
 
         return R.success("添加成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(Integer page, Integer limit){
+        log.info("page = {},pageSize = {},name = {}" ,page,limit);
+        //构造分页构造器
+        Page pageInfo = new Page(page,limit);
+        //执行查询
+        service.page(pageInfo);
+
+        return R.success(pageInfo);
     }
 }
