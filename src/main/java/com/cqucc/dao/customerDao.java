2d017d78a6ee076d538333990188cf77cb3ab522
asgard <1792181243@qq.com>
2022-11-24 11:28:04 +0800

    客户 dao接口

--- src/main/java/com/cqucc/dao/customerDao.java
+++ src/main/java/com/cqucc/dao/customerDao.java
@@ -2,8 +2,17 @@ package com.cqucc.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cqucc.pojo.Customer;
+import com.cqucc.pojo.Report;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface customerDao extends BaseMapper<Customer> {
+
+    @Select("select count(*) as num,cus_region as item\n" +
+            "from t_customer group by cus_region;")
+    List<Report> countRegion();
+
 }
