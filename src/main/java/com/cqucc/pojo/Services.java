3a21f7fba46856d6d7beaf9bbd8cb5b97b1ae0ea
asgard <1792181243@qq.com>
2022-11-26 20:58:38 +0800

    服务实体类

--- src/main/java/com/cqucc/pojo/Services.java
+++ src/main/java/com/cqucc/pojo/Services.java
@@ -8,32 +8,30 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@TableName(value="t_services")
+@TableName(value = "t_services")
 @Data
 public class Services {
-   private Integer serId;
-   private Integer serType;
-   private String serDescription;
-   private Integer cusId;
-   private Integer serCreater;
-   private Integer serHandle;
-   private String serHandleperson;
-   private String serNote;
-   private Integer serSatisfy;
-   private LocalDateTime serTime;
-   private String serDetail;
-   private Integer serStatus;
+    private Integer serId;
+    private Integer serType;
+    private String serDescription;
+    private Integer cusId;
+    private Integer serCreater;
+    private Integer serHandle;
+    private String serHandleperson;
+    private String serNote;
+    private Integer serSatisfy;
+    private LocalDateTime serTime;
+    private String serDetail;
+    private Integer serStatus;
 
-   @TableField(fill = FieldFill.INSERT) //创建时填充字段
-   private LocalDateTime serCreatetime;
+    @TableField(fill = FieldFill.INSERT) //创建时填充字段
+    private LocalDateTime serCreatetime;
 
-   @TableField(fill = FieldFill.INSERT)//提交时填充字段
-   private LocalDateTime serGivetime;
+    @TableField(fill = FieldFill.INSERT)//提交时填充字段
+    private LocalDateTime serGivetime;
 
-   @TableField(fill = FieldFill.INSERT_UPDATE)//插入和更新时填充字段
-   private LocalDateTime serHandletime;
-
-
+    @TableField(fill = FieldFill.INSERT_UPDATE)//插入和更新时填充字段
+    private LocalDateTime serHandletime;
 
 
 }
