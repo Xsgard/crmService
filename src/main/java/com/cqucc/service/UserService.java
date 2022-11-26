7427b4a7d8493d7b31e3e46461406689e8e48fa4
asgard <1792181243@qq.com>
2022-11-26 20:59:44 +0800

    User service 接口

--- src/main/java/com/cqucc/service/UserService.java
+++ src/main/java/com/cqucc/service/UserService.java
@@ -1,10 +1,11 @@
 package com.cqucc.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.cqucc.pojo.Sort;
 import com.cqucc.pojo.User;
 
 import java.util.List;
 
 public interface UserService extends IService<User> {
-    List<User> getIdAndName();
+    List<Sort> getIdAndName();
 }
